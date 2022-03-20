16bf4a4b521b8ed832f4cb76436abf8d338afe27
Kris Smith <91396141+KrisSmith7@users.noreply.github.com>
2022-03-20 17:43:43 -0400

    returns data from an API call and renders to page

--- client/src/Components/Pages/Resources.js
+++ client/src/Components/Pages/Resources.js
@@ -1,10 +1,56 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 function Resources (){
+        const [error, setError] = useState(null);
+        const [isLoaded, setIsLoaded] = useState(false);
+        const [items, setItems] = useState([]);
+      
+        // Note: the empty deps array [] means
+        // this useEffect will run once
+        // similar to componentDidMount()
+        useEffect(() => {
+          fetch("https://api.open5e.com/spells/")
+            .then(res => res.json())
+            .then(
+                (results) => {
+                  console.log([results])
+                setIsLoaded(true);
+                setItems(results.results);
+                console.log(items)
+              },
+              // Note: it's important to handle errors here
+              // instead of a catch() block so that we don't swallow
+              // exceptions from actual bugs in components.
+              (error) => {
+                setIsLoaded(true);
+                setError(error);
+              }
+            )
+        }, [])
+      
+        if (error) {
+          return (<div>Error: {error.message}</div>)
+        }
+        
+        if (!isLoaded) {
+          return (
+          <div>Loading...</div>)
+          }
+console.log ([items]);
+          return (
+              
+              <section className="bg-charcoal h-full">
+                  
+            <ul>
+              {items.map((item, i) => (
+                  <li key={item[i]}>
+                  {item.name}
+                </li>
+               ))}
+            </ul>
+              </section>
+          );
+        }
 
-    return (
-    <h1> Use API here for questions/character info? </h1>
-    )
-}
 
 export default Resources;
