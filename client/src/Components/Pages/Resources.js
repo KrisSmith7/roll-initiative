3b232e25b2ecae576f7d5a88a487c813177f892f
Kris Smith <91396141+KrisSmith7@users.noreply.github.com>
2022-03-20 20:57:30 -0400

    returns key and value for each object returned with api call

--- client/src/Components/Pages/Resources.js
+++ client/src/Components/Pages/Resources.js
@@ -3,28 +3,27 @@ import React, {useState, useEffect} from "react";
 function Resources (){
         const [error, setError] = useState(null);
         const [isLoaded, setIsLoaded] = useState(false);
-        const [items, setItems] = useState([]);
+        const [items, setItems] = useState([{}]);
       
         // Note: the empty deps array [] means
         // this useEffect will run once
         // similar to componentDidMount()
         useEffect(() => {
-          fetch("https://api.open5e.com/spells/")
+          fetch("https://api.open5e.com/spells/?limit=5")
             .then(res => res.json())
             .then(
                 (results) => {
-                  console.log([results])
-                setIsLoaded(true);
-                setItems(results.results);
-                console.log(items)
-              },
-              // Note: it's important to handle errors here
-              // instead of a catch() block so that we don't swallow
-              // exceptions from actual bugs in components.
-              (error) => {
+                    setIsLoaded(true);
+                    setItems(results.results);
+                    // console.log(items)
+            },
+            // Note: it's important to handle errors here
+            // instead of a catch() block so that we don't swallow
+            // exceptions from actual bugs in components.
+            (error) => {
                 setIsLoaded(true);
                 setError(error);
-              }
+            }
             )
         }, [])
       
@@ -34,20 +33,29 @@ function Resources (){
         
         if (!isLoaded) {
           return (
-          <div>Loading...</div>)
-          }
-console.log ([items]);
-          return (
-              
-              <section className="bg-charcoal h-full">
-                  
-            <ul>
-              {items.map((item, i) => (
-                  <li key={item[i]}>
-                  {item.name}
-                </li>
-               ))}
-            </ul>
+              <div>Loading...</div>)
+            }
+
+         return (
+             
+             <section className="bg-charcoal h-full">
+               <div className="h-1/2">
+            {items.map(item =>
+                 { return (
+                //   console.log(item)
+                     Object.keys(item).map((key,i) => (
+                            // console.log(key,i,item[key])
+
+                   <p key={i} className="py-2">
+                       <span className="uppercase">{key}: </span>
+                       <span className="normal-case"> {item[key]}</span>
+                       </p>
+               ))
+                 )
+               }
+            )}
+
+                   </div> 
               </section>
           );
         }
