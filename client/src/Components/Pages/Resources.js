629d9d040122e97c830a3ebf827cf11e602b3ded
Kris Smith <91396141+KrisSmith7@users.noreply.github.com>
2022-03-21 15:34:13 -0400

    useEffect displays url that changes based on button selected

--- client/src/Components/Pages/Resources.js
+++ client/src/Components/Pages/Resources.js
@@ -5,26 +5,37 @@ function Resources() {
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
     const [items, setItems] = useState([{}]);
-    // const handleInput = (evt) => {
-    //     const searchItem = evt.target.value
-    //     console.log(evt.target.value)
-    // } 
+    const [url, setURL] = useState(("https://api.open5e.com/weapons"))
+
+    const handleInput = (evt) => {
+        const searchItem = evt.target.value
+        // console.log(searchItem)
+        setURL(`https://api.open5e.com/${searchItem}`)
+        // console.log(url);
+    }
 
     useEffect(() => {
-        fetch(`https://api.open5e.com/weapons`)
-            .then(res => res.json())
-            .then(
-                (results) => {
-                    setIsLoaded(true);
-                    setItems(results.results);
-                    console.log(items)
-                },
-                (error) => {
-                    setIsLoaded(true);
-                    setError(error);
-                }
-            )
-    }, [])
+        const fetchData = async () => {
+            const urlData = await fetch(url)
+                .then(res => res.json())
+                .then(
+                    (results) => {
+                        setIsLoaded(true);
+                        setTimeout(() => {
+                            setItems(results.results)
+                        }, 500)
+                        // console.log(items)
+                    },
+                    (error) => {
+                        setIsLoaded(true);
+                        setError(error);
+                    }
+                )
+        }
+
+        fetchData()
+            .catch(console.error)
+    }, [handleInput])
 
     if (error) {
         return (<div>Error: {error.message}</div>)
@@ -44,15 +55,15 @@ function Resources() {
             </div>
 
             <div className="md:py-6 w-full flex flex-wrap text-white">
-                <button value={"spells"} onClick={evt=>this.handleInput(evt)} className="h-1/2 w-full md:w-1/5 p-2.5 mx-2.5 my-1.5 bg-sienna rounded-lg uppercase md:font-bold">Spells</button>
-                <button value={"backgrounds"} onClick={evt=>this.handleInput(evt)} className="h-1/2 w-full md:w-1/5 p-2.5 mx-2.5 my-1.5 bg-sienna rounded-lg uppercase md:font-bold">Backgrounds</button>
-                <button value={"races"} onClick={evt=>this.handleInput(evt)} className="h-1/2 w-full md:w-1/5 p-2.5 mx-2.5 my-1.5 bg-sienna rounded-lg uppercase md:font-bold">Races</button>
-                <button value={"classes"} onClick={evt=>this.handleInput(evt)} className="h-1/2 w-full md:w-1/5 p-2.5 mx-2.5 my-1.5 bg-sienna rounded-lg uppercase md:font-bold">Classes</button>
-                <button value={"magicitems"} onClick={evt=>this.handleInput(evt)} className="h-1/2 w-full md:w-1/5 p-2.5 mx-2.5 my-1.5 bg-sienna rounded-lg uppercase md:font-bold">Magic Items</button>
-                <button value={"weapons"} onClick={evt=>this.handleInput(evt)} className="h-1/2 w-full md:w-1/5 p-2.5 mx-2.5 my-1.5 bg-sienna rounded-lg uppercase md:font-bold">Weapons</button>
+                <button value={"spells"} onClick={evt => handleInput(evt)} className="h-1/2 w-full md:w-1/5 p-2.5 mx-2.5 my-1.5 bg-sienna rounded-lg uppercase md:font-bold">Spells</button>
+                <button value={"backgrounds"} onClick={evt => handleInput(evt)} className="h-1/2 w-full md:w-1/5 p-2.5 mx-2.5 my-1.5 bg-sienna rounded-lg uppercase md:font-bold">Backgrounds</button>
+                {/* <button value={"races"} onClick={evt=>handleInput(evt)} className="h-1/2 w-full md:w-1/5 p-2.5 mx-2.5 my-1.5 bg-sienna rounded-lg uppercase md:font-bold">Races</button> */}
+                {/* <button value={"classes"} onClick={evt=>handleInput(evt)} className="h-1/2 w-full md:w-1/5 p-2.5 mx-2.5 my-1.5 bg-sienna rounded-lg uppercase md:font-bold">Classes</button> */}
+                <button value={"magicitems"} onClick={evt => handleInput(evt)} className="h-1/2 w-full md:w-1/5 p-2.5 mx-2.5 my-1.5 bg-sienna rounded-lg uppercase md:font-bold">Magic Items</button>
+                <button value={"weapons"} onClick={evt => handleInput(evt)} className="h-1/2 w-full md:w-1/5 p-2.5 mx-2.5 my-1.5 bg-sienna rounded-lg uppercase md:font-bold">Weapons</button>
             </div>
 
-            <div className="h-3/5 bg-sienna overflow-auto">
+            <div className="h-fit-content bg-sienna overflow-auto md:flex">
                 {items.map(item =>
                     <div className="bg-gray-900/[.35] mb-4 lg:mx-4 py-4">
                         <h1 className="text-3xl text-center">{item.name}</h1>
@@ -61,7 +72,7 @@ function Resources() {
                             Object.keys(item).map((key, i) => (
                                 // console.log(key,i,item[key])
                                 <div key={i} className="px-2 lg:py-2 lg:px-4 lg:text-xl text-white leading-loose">
-                                    <span className="capitalize font-light lg:pr-8">{key}: </span>
+                                    <span className="capitalize font-light text-sm lg:pr-4">{key}:</span>
                                     <span className="font-semibold tracking-widest font-unicase"> {item[key]}</span>
                                 </div>
                             ))
