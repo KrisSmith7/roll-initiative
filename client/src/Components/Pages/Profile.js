99f226c4c26e041c35395edac411e6d2684c968b
acdodd17 <acdodd17@gmail.com>
2022-03-21 15:47:42 -0400

    flex adjust profile header

--- client/src/Components/Pages/Profile.js
+++ client/src/Components/Pages/Profile.js
@@ -40,9 +40,12 @@ function Profile (props){
     return (
        <div className="flex justify-center">
            <div className="w-4/5">
-                <div className=" flex justify-center user-profile mt-3 bg-gradient-to-b from-charcoal/[.35] items-center">
-                    <img className="rounded-full w-60 p-3" src={Die} alt="profile-img"></img>
-                    <div> 
+                <div className=" flex justify-evenly user-profile mt-3 bg-gradient-to-b from-charcoal/[.35] items-center">
+                    <div className="flex-auto rounded-full w-20 p-3">
+                        <img className="p-4" src={Die} alt="profile-img"></img>
+                    </div>
+                    
+                    <div className="flex-auto w-80"> 
                         <h2 className="text-5xl text-slate font-antiqua"> {user.username ? `${user.username}'s` : 'your'} profile </h2>
                         <button className='w-50 text-slate bg-turq/75 rounded-md mt-5'> ✏️ Edit Profile</button>
                     </div>
