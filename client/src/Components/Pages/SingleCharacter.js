c420c27396ff244310ff7b5401f936f4e41e03d4
Poke Code <90790144+aucoats@users.noreply.github.com>
2022-03-21 18:25:35 -0400

    final updateCharacter sans useEffect and some styles

--- client/src/Components/Pages/SingleCharacter.js
+++ client/src/Components/Pages/SingleCharacter.js
@@ -23,7 +23,7 @@ function SingleCharacter() {
     });    
     
     const character = data?.character;
-    //console.log('character data: ', character);
+    console.log('character data: ', character);
 
     const [characterForm, setCharacterForm] = useState({
         name: '',
@@ -40,12 +40,6 @@ function SingleCharacter() {
         wis: '',
         cha: ''
     });
-
-    useEffect(() => {
-            setCharacterForm({
-                name: character?.name, 
-                })
-    }, []); 
 
     const [deleteCharacter, { error }] = useMutation(DELETE_CHARACTER, {
         update(cache, { data: { deleteCharacter }}) {
@@ -93,33 +87,7 @@ function SingleCharacter() {
         routeChange();
     }
 
-    const [updateCharacter, { updateError }] = useMutation(UPDATE_CHARACTER, {
-        // This part doesn't need to be here since when the character is updated it automatically
-        // updates the cache.
-
-
-        // update(cache, { data: { updateCharacter }}) {
-        //     console.log(updateCharacter); 
-
-        //     try {
-        //         const { characters } = cache.readQuery({ query: QUERY_CHARACTER }); 
-        //         cache.writeQuery({
-        //             query: QUERY_CHARACTER, 
-        //             data: { characters: [...characters].filter((character) => character._id !== updateCharacter._id).then(characters.push(updateCharacter))}
-        //         });
-        //     }
-        //     catch (err) {
-        //         console.error(err); 
-        //     }
-
-        //     const { me } = cache.readQuery({ query: QUERY_ME });
-        //     console.log(me);
-        //     cache.writeQuery({
-        //       query: QUERY_ME,
-        //       data: { me: { ...me, characters: [...me.characters].filter((character) => character._id !== deleteCharacter._id).then(me.characters.push(updateCharacter)) } }
-        //     });
-        // }
-    } )
+    const [updateCharacter, { updateError }] = useMutation(UPDATE_CHARACTER);
 
     const characterChangeHandler = (e) => { 
         //console.log(e.target.name);
@@ -147,14 +115,6 @@ function SingleCharacter() {
         };
         console.log("updatedCharacter", updatedCharacter);
         try {
-            // characterForm.level = parseInt(characterForm.level); 
-            // characterForm.str = parseInt(characterForm.str);
-            // characterForm.dex = parseInt(characterForm.dex); 
-            // characterForm.con = parseInt(characterForm.con); 
-            // characterForm.wis = parseInt(characterForm.wis); 
-            // characterForm.int = parseInt(characterForm.int); 
-            // characterForm.cha = parseInt(characterForm.cha); 
-
             await updateCharacter({
                 variables: { 
                 _id: id,
@@ -189,33 +149,32 @@ function SingleCharacter() {
     }
 
     return(
-        <div className="flex flex-col items-center tracking-wider text-md p-4 lg:text-xl lg:p-8"> 
+        <div className="flex flex-col items-center tracking-wider text-md p-4 lg:text-xl lg:p-8 text-rose-900"> 
             <div className='flex justify-center'>
                 <Link to="/profile" className='text-slate font-bold mt-5'> ← Back to Profile </Link>
             </div>
             <Page>
                 <PageTitle>{character.name}</PageTitle>
-                <Note>
-                    <h1>Class</h1><textarea defaultValue={character.class} name="class" onBlur={characterChangeHandler}></textarea>
-                    <h1>Race</h1><textarea defaultValue={character.race} name="race" onBlur={characterChangeHandler}></textarea>
-                    <h1>Level</h1><textarea defaultValue={character.level} name="level" onBlur={characterChangeHandler}></textarea>
-                    <h1>Background</h1><textarea defaultValue={character.background} name="background" onBlur={characterChangeHandler}></textarea>
-                    <h1>Bio</h1><textarea defaultValue={character.bio} name="bio" onBlur={characterChangeHandler}></textarea>
+                <Note className='flex flex-col text-xl text-center'> 
+                    <h1>Class</h1><textarea className='bg-inherit w-1/2 m-auto pl-8' defaultValue={character.class} name="class" onBlur={characterChangeHandler}></textarea>
+                    <h1>Race</h1><textarea className='bg-inherit w-1/2 m-auto pl-8' defaultValue={character.race} name="race" onBlur={characterChangeHandler}></textarea>
+                    <h1>Level</h1><textarea className='bg-inherit w-1/2 m-auto pl-8' defaultValue={character.level} name="level" onBlur={characterChangeHandler}></textarea>
+                    <h1>Background</h1><textarea className='bg-inherit w-1/2 m-auto pl-8' defaultValue={character.background} name="background" onBlur={characterChangeHandler}></textarea>
+                    <h1>Bio</h1> <textarea  className='bg-inherit w-1/2 m-auto pl-8 overflow-auto' defaultValue={character.bio} onBlur={characterChangeHandler}></textarea>
                 </Note>
-                <StatBlock>
-                    <h1>Strength</h1><textarea defaultValue={character.str} name='str' onBlur={characterChangeHandler}></textarea>
-                    <h1>Dexterity</h1><textarea defaultValue={character.dex} name='dex' onBlur={characterChangeHandler}></textarea>
-                    <h1>Constitution</h1><textarea defaultValue={character.con} name='con' onBlur={characterChangeHandler}></textarea>
-                    <h1>Wisdom</h1><textarea defaultValue={character.wis} name='wis' onBlur={characterChangeHandler}></textarea>
-                    <h1>Intelligence</h1><textarea defaultValue={character.int} name='int' onBlur={characterChangeHandler}></textarea>
-                    <h1>Charisma</h1><textarea defaultValue={character.cha} name='cha' onBlur={characterChangeHandler}></textarea>
+                <StatBlock className='flex flex-col text-xl text-center'>
+                    <h1>Strength</h1><textarea  className='bg-inherit w-1/2 m-auto pl-8'defaultValue={character.str} name='str' onBlur={characterChangeHandler}></textarea>
+                    <h1>Dexterity</h1><textarea  className='bg-inherit w-1/2 m-auto pl-8'defaultValue={character.dex} name='dex' onBlur={characterChangeHandler}></textarea>
+                    <h1>Constitution</h1><textarea  className='bg-inherit w-1/2 m-auto pl-8'defaultValue={character.con} name='con' onBlur={characterChangeHandler}></textarea>
+                    <h1>Wisdom</h1><textarea className='bg-inherit w-1/2 m-auto pl-8' defaultValue={character.wis} name='wis' onBlur={characterChangeHandler}></textarea>
+                    <h1>Intelligence</h1><textarea className='bg-inherit w-1/2 m-auto pl-8' defaultValue={character.int} name='int' onBlur={characterChangeHandler}></textarea>
+                    <h1>Charisma</h1><textarea className='bg-inherit w-1/2 m-auto pl-8' defaultValue={character.cha} name='cha' onBlur={characterChangeHandler}></textarea>
                 </StatBlock>
-                <MagicItem title={'Biography'}>
-                    <p>{character.bio}</p>
-                </MagicItem>
             </Page>
-            <button onClick={() => handleCharacterDelete(character._id)}>Delete {character.name}?</button>
-            <button onClick={() => handleCharacterUpdate(character._id)}>Save Changes to {character.name}</button>
+            <button className='text-white bg-emerald-900 p-2 m-2 rounded-md'
+                onClick={() => handleCharacterUpdate(character._id)}>Save Changes to {character.name}</button>
+            <button className='text-white bg-sienna/75 p-2 m-2 rounded-md' 
+                onClick={() => handleCharacterDelete(character._id)}>Delete {character.name}?</button>
            
 
         </div>
