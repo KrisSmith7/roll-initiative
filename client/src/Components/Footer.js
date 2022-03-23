bc4871e197eac2dae103eac4936cfd0f49269617
acdodd17 <acdodd17@gmail.com>
2022-03-23 10:55:03 -0400

    welcome link to profile and die roll in footer

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,11 +1,14 @@
 import React from "react";
+import logo from "../assets/socialrolls_logo.png";
+
 
 function Footer (){
 
     return (
         <div>
             <div className="push"></div>
-            <footer className="bg-charcoal w-full text-center p-3" style={{zIndex:1}}> Social Rolls </footer>
+            
+            <footer className=" flex justify-center items-center bg-charcoal w-full font-antiqua text-center text-white p-3" style={{zIndex:1}}> <img src={logo} className=" roll-in h-8 mr-5" alt="20-sided die" /> Social Rolls </footer>
         </div> 
         
     )
